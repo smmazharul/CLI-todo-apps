4bf17ebb4505329ff11ab9e17f81c92d67ddd105
smmazharul <hmoxygen@gmail.com>
2025-04-09 15:13:12 +0600

    create a function to select option and create todos and view todos

--- server.js
+++ server.js
@@ -15,12 +15,25 @@ const showMenu = ()=>{
     rl.question("Choose an option:",handleInput);
 }
 const handleInput =(option)=>{
-    if(option === 1){
+    if(option === "1"){
         rl.question("Enter the task:", (task)=>{
             todos.push(task)
             console.log("Task added:" , task)
             showMenu()
         })
+    }
+    else if(option === "2"){
+        console.log("\n your todo list")
+       todos.forEach((task,index)=>{
+        console.log(`${index+1}. ${task}`)
+       })
+       showMenu()
+    }else if(option==="3"){
+        console.log("Good Bye");
+        rl.close()
+    }else{
+        console.log("Invalid Option please try again");
+        showMenu()
     }
 }
 
